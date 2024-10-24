6ddca6d7423dd3d6850f7d96a0fff29c047e65ac
Zazary <110578041+ZazaryX@users.noreply.github.com>
2024-10-24 23:54:43 +0700

    4

---
+++ main.js
@@ -0,0 +1,5 @@
+HTML SCSS JSResult Skip Results Iframe
+EDIT ON
+document.querySelector("#retrobg-sun").onclick = () => {
+    document.querySelector("#retrobg").classList.toggle("retrobg-shutdown");
+};
